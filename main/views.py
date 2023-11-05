14e6b2bffdc25d2b3de238e8b46b72c2fc57c69c
doniyor <suratovdoniyor@gmail.com>
2023-11-05 09:57:23 +0500

    initial commit

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+# Create your views here.
